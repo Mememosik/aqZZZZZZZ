f5ee8fccc31289e55f4007e172f3273a9bcc6a6e
Mememosik <zaisunovnikita@gmail.com>
2025-10-15 13:56:59 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementyById('clickBtn').addEventListener('click'), () => {
+  const message =[
+    'Отлично! Сайт работает!',
+    'Вы великолепны!',
+    'Это работает в интернете!'
+    '
+    const randomMessage = message [Math.floor[(ath.random() *
+      messages.length)];
+  document.getElementById('message').textContent = randomMessage;
+    
+  ]};
+  
+
